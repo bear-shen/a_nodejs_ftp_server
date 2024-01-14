fd2bf76b90d686b8505f115d7befc129ae1e7eff
shen <bear_shen@live.cn>
2024-01-14 18:03:24 +0800

    noop

--- method/NOOP.ts
+++ method/NOOP.ts
@@ -1,4 +1,6 @@
 import {SessionDef} from "../types";
+import {buildTemplate} from "../Lib";
 
 export async function execute(session: SessionDef, buffer: Buffer) {
+    return session.socket.write(buildTemplate(200));
 }
